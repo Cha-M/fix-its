2a1322d7dfdadc2a3e3c16e9cbca1d7a857b00ff
Sha <charlotte.megroff@outlook.com>
2022-02-15 16:47:09 +0000

    Exercise 2

--- src/Fix-it/Fix-it-2.js
+++ src/Fix-it/Fix-it-2.js
@@ -1,15 +1,17 @@
 const FixIt2 = () => {
+    return (
 
         <>
             <Book num="1" title="Ready Player One" author="Ernest Cline" />
             <Book num="2" title="The Hobbit" author=" J R R Tolkien" />
             <Book num="3" title="Do Androids Dream Of Electric Sheep?" author="Philip K. Dick" />
         </>
-
+    )
 }
 
 const Book = (props) => {
     return (
+        
         <div>
             <h2>number: {props.num}</h2>
             <h2>title: {props.title}</h2>
